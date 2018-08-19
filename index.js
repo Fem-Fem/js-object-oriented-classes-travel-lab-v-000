c0829f7153f63fa327bc6ec4f59910bd88f42849
Obafemi Fadairo <ofpupp@princeton.edu>
2018-08-19 00:55:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ class Driver {
 
   startDate() {
     this.startDate = new Date(this.string)
-    return this.startDate
+    return this.startDate.class
   }
 
   yearsExperienceFromBeginningOf(year) {
