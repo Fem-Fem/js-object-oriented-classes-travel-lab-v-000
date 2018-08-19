113bb641e7b324eb6394d2590e15a71c7a569aff
Obafemi Fadairo <ofpupp@princeton.edu>
2018-08-19 00:27:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ class Route {
     this.endingLocation = endingLocation
   }
   blocksTravelled() {
-    return `${this.beginningLocation}`[horizontal]
+    return `${this.beginningLocation}`[2]
 
   }
   estimatedTime() {
