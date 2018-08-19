9b465944c452b2f239199e734cb54a62ec6c358a
Obafemi Fadairo <ofpupp@princeton.edu>
2018-08-19 00:22:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,8 @@
 
 class Route {
   constructor(beginningLocation, endingLocation) {
-
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
   }
   blocksTravelled() {
 
